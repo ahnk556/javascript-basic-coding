d3aa30eff6cacc1ef925d031f590878f74ca6ca0
ahnk556 <ahnk556@newschool.edu>
2019-09-06 15:38:52 -0400

    initial commit

--- week1/week1.js
+++ week1/week1.js
@@ -14,9 +14,10 @@ console.log(division);
 
 /* Problem 2 */
 /* Write a JavaScript program to get the area of a triangle where the height is 4, the width is 2. */
-
+// Write your code here
 
 
 
 /* Program 4 */
 /* Write a JavaScript program to convert temperatures from Celsius to Fahrenheit */
+// Write your code here
