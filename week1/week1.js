93a536e78ffb0338f011a3eb73f797c8774a6742
ahnk556 <ahnk556@newschool.edu>
2019-09-06 15:36:41 -0400

    initial commit

---
+++ week1/week1.js
@@ -0,0 +1,22 @@
+/* Problem 1 */
+var name = "John";
+console.log(name);
+
+/* Problem 3 */
+var num1 = 2;
+var num2 = 4;
+
+var product = num1 * num2;
+console.log(product);
+
+var division = num1 / num2;
+console.log(division);
+
+/* Problem 2 */
+/* Write a JavaScript program to get the area of a triangle where the height is 4, the width is 2. */
+
+
+
+
+/* Program 4 */
+/* Write a JavaScript program to convert temperatures from Celsius to Fahrenheit */
