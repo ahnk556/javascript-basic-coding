de2965caa66a32cb9ada39edc5722bc5a3e17d3b
ahnk556 <ahnk556@newschool.edu>
2019-09-10 16:51:59 -0400

    code added

--- week1/week1.js
+++ week1/week1.js
@@ -15,9 +15,34 @@ console.log(division);
 /* Problem 2 */
 /* Write a JavaScript program to get the area of a triangle where the height is 4, the width is 2. */
 // Write your code here
+var width = 2;
+var height = 4;
 
+var area = (width * height) / 2;
+console.log(area);
 
 
 /* Program 4 */
 /* Write a JavaScript program to convert temperatures from Celsius to Fahrenheit */
 // Write your code here
+
+// var c = 30;
+// var f = c*(9/5)+32;
+// console.log(f);
+
+// function convert (c){
+// return c*(9/5)+32;
+// }
+
+function convert (celsius){
+    var f = celsius*(9/5)+32;
+    return f;
+}
+ 
+function area (r){
+    var area = 3.14*r*r
+    return area;
+}
+
+
+
